136407fa357a4a1e698415820df640749fa8e7d4
karino-t <karino-t@klab.com>
2014-08-03 13:55:02 +0900

    fix run

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ from model_params import model_params
 
 from collections import defaultdict
 
+from run_direct import Evaluate
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
@@ -93,89 +94,6 @@ class RegionState(object):
 
 
 
-
-class Evaluate(object):
-    """
-    high/low評価用
-    """
-
-    def __init__(self, number=1):
-        self.number = number
-        self.ok_count = [0 for i in range(self.number)]
-        self.ng_count = [0 for i in range(self.number)]
-        self.color = { 'green': '\033[32m',
-                       'yellow': '\033[33m',
-                       'red': '\033[31m',
-                       'clear': '\033[0m'}
-
-    def reset(self):
-        self.ok_count = [0 for i in range(self.number)]
-        self.ng_count = [0 for i in range(self.number)]
-
-    def evaluate_one(self, before, now, predict_list, before_predvalue):
-        """
-        1パラメータによるhigh/low予測
-
-        close値のみの予測を想定.
-
-        predict_list = [value, value, value...]
-        """
-        res  = 'high' if now > before  else 'low'
-
-        print "%5.2f -> %5.2f, %5s, " % (before, now, res) ,
-        for i, p_val in enumerate(predict_list):
-            pred = 'high' if p_val > before_predvalue[i] else 'low'
-
-            if res == pred:
-                evalue = self.color['green'] + 'OK' + self.color['clear']
-                self.ok_count[i] += 1
-            else:
-                evalue = self.color['red'] + 'NG' + self.color['clear']
-                self.ng_count[i] += 1
-
-            ok_rate = float(self.ok_count[i])/(self.ok_count[i] + self.ng_count[i])
-            print "%5s, %10s, %5.4f" % (pred, evalue, ok_rate) ,
-        print
-
-
-    # TODO: 現状動かなくなっているから直す.
-    def evaluate(self, before, now, high_pred, low_pred):
-        """
-        2パラメータによるhigh/low予測
-
-        high/low値の予測を想定
-         predict_list = [(high, low), (high, low)....]
-        """
-        res = 'high' if now - before > 0 else 'low'
-        print "%5.2f -> %5.2f, %5s, " % (before, now, res) ,
-
-        for i, pred_data in enumerate(predict_list):
-            # high/low predict
-            if  before< pred_data[1]:
-                pred = 'high'
-            elif  before > pred_data[0]:
-                pred = 'low'
-            else:
-                pred = 'not'
-
-            # evalueate
-            if pred == 'not':
-                evalue = self.color['yellow'] + 'NO' + self.color['clear']
-            elif res == pred:
-                evalue = self.color['green'] + 'OK' + self.color['clear']
-                self.ok_count[i] += 1
-            else:
-                evalue = self.color['red'] + 'NG' + self.color['clear']
-                self.ng_count[i] += 1
-
-            if pred == 'not':
-                print "%5s, %10s" % (pred, evalue)
-            else:
-                ok_rate = float(self.ok_count[i])/(self.ok_count[i] + self.ng_count[i])
-                print "%5s, %10s, %5.4f" % (pred, evalue, ok_rate)
-        print
-
-
 def createModel(target):
     model = ModelFactory.create(model_params.MODEL_PARAMS)
     model.enableInference({
@@ -201,18 +119,18 @@ def get_formated_data(row):
     high_value = float(row[2])
     low_value  = float(row[3])
     close_value  = float(row[4])
-    now_value = (high_value + low_value)/2
 
     return {'timestamp': timestamp,
             # 'open': open_value,
             # 'high': high_value,
             'low': low_value,
             # 'close': close_value,
-            } , now_value
+            }
+
 
 def runModel(inputFilePath, high_model, low_model, print_result=True):
 
-    ep = Evaluate(number=3)
+    ep = Evaluate(predict_number=1)
     region_sate = RegionState()
 
     csv_data = get_csv_data(inputFilePath)
@@ -230,13 +148,17 @@ def runModel(inputFilePath, high_model, low_model, print_result=True):
     for i in range(1):
         for row in csv_data:
             counter += 1
-            if (counter % 720 == 0) :
-                print i, data['timestamp'],
-                print "Read %i lines..." % counter
-                ep.reset()      # high/low予測結果のリセット.
-                counter = 0
+            # if (counter % 10 == 0) :
+            #     tp = low_model._getTPRegion()
+            #     tp.getSelf().resetSequenceStates()
 
-            data, now_value = get_formated_data(row)
+            data  = get_formated_data(row)
+            now_low_value = data['low']
+
+            # if before_value:
+            #     data['low'] -= before_value
+            # else:
+            #     data['low'] = 0
 
             # run cla model
             #high_result = high_model.run(data)
@@ -266,8 +188,6 @@ def runModel(inputFilePath, high_model, low_model, print_result=True):
                     print i,counter,
                     ep.evaluate_one(before_value, data['low'], [low_prediction, low_prediction_4, low_prediction_24], before_predvalue)
 
-                # save now_value
-                #before_value = now_value
                 # TODO: ここ間違ってる. low_prediction_4は４時間前, low_prediction_24は24時間前のものが必要.
                 # InferenceShifter を上手く使えるかな?
                 before_value     = data['low']
@@ -282,27 +202,29 @@ def runModel(inputFilePath, high_model, low_model, print_result=True):
 
 def binary_option():
     from nupic.frameworks.opf.model import Model
-    inputFilePath = "./datasets/usdjpy_2001_01_ohlc.csv"
+    #inputFilePath = "./datasets/usdjpy_2001_01_ohlc.csv"
     #inputFilePath = "./datasets/usdjpy_2001_ohlc.csv"
     #inputFilePath = "./datasets/usdjpy_2001_2005_ohlc.csv"
-    #inputFilePath = "./datasets/usdjpy_2006_2007.csv"
+    inputFilePath = "./datasets/usdjpy_2006_2007.csv"
 
-    print 'create model ...'
     # TODO: 学習したモデルの保存ができてない.
     if os.path.exists('./learned_model'):
-        high_model = Model.load('./learned_model/high/')
+        print 'loading model ...'
+        #high_model = Model.load('./learned_model/high/')
         low_model  = Model.load('./learned_model/low/')
     else:
-        high_model = createModel("high")
+        print 'create model ...'
+        #high_model = createModel("high")
         low_model  = createModel("low")
 
+    high_model = createModel("high")
 
     print 'run Model ...'
     runModel(inputFilePath, high_model, low_model, True)
 
-    print 'pickle dump ...'
-    high_model.save(os.path.abspath('./learned_model/high/'))
-    low_model.save(os.path.abspath('./learned_model/low/'))
+    # print 'pickle dump ...'
+    # high_model.save(os.path.abspath('./learned_model/high/'))
+    # low_model.save(os.path.abspath('./learned_model/low/'))
 
 
 if __name__ == "__main__":
